f4808fecf5ec4db0bb9ab7e421a8c03011b8bbdc
ijolidino <31976195+ijolidino@users.noreply.github.com>
2021-01-14 15:53:30 +0800

    Update BlackmagicThirdPartyApplicationTests.java

--- blackmagic-third-party/src/test/java/com/blackmagic/third/party/thirdparty/BlackmagicThirdPartyApplicationTests.java
+++ blackmagic-third-party/src/test/java/com/blackmagic/third/party/thirdparty/BlackmagicThirdPartyApplicationTests.java
@@ -19,10 +19,10 @@ class BlackmagicThirdPartyApplicationTests {
 	@Test
 	public void testUpload() throws FileNotFoundException {
 		// Endpoint以杭州为例，其它Region请按实际情况填写。
-		String endpoint = "oss-cn-guangzhou.aliyuncs.com";
+		String endpoint = "";
 		// 云账号AccessKey有所有API访问权限，建议遵循阿里云安全最佳实践，创建并使用RAM子账号进行API访问或日常运维，请登录 https://ram.console.aliyun.com 创建。
-		String accessKeyId = "LTAI4GFZxpchTdCuYAmT21Gs";
-		String accessKeySecret = "HX0DXigOYEHsjVwlLFrKFXmgj1JdcN";
+		String accessKeyId = "";
+		String accessKeySecret = "";
 
 		// 创建OSSClient实例。
 		OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
