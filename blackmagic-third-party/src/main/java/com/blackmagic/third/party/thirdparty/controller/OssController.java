1a79118c57bde1640a0c7c19d4f6d56844f5d676
ijolidino <31976195+ijolidino@users.noreply.github.com>
2021-05-10 21:27:27 +0800

    Update OssController.java

--- blackmagic-third-party/src/main/java/com/blackmagic/third/party/thirdparty/controller/OssController.java
+++ blackmagic-third-party/src/main/java/com/blackmagic/third/party/thirdparty/controller/OssController.java
@@ -38,8 +38,8 @@ public class OssController {
         // Endpoint以杭州为例，其它Region请按实际情况填写。
         String endpoint = "oss-cn-guangzhou.aliyuncs.com";
         // 云账号AccessKey有所有API访问权限，建议遵循阿里云安全最佳实践，创建并使用RAM子账号进行API访问或日常运维，请登录 https://ram.console.aliyun.com 创建。
-        String accessKeyId = "LTAI4GFZxpchTdCuYAmT21Gs";
-        String accessKeySecret = "HX0DXigOYEHsjVwlLFrKFXmgj1JdcN";
+        String accessKeyId = "LTAI4G1GsxIqrwFZxpchTdCuYAmT21GsxIqrw46";
+        String accessKeySecret = "HX0gj1JdcNNxijDXigOYEHsjVwlLFrKFXmgj1JdcNNxijqWtR";
 
         String bucket = "blackmagic"; // 请填写您的 bucketname 。
         String host = "https://" + bucket + "." + endpoint; // host的格式为 bucketname.endpoint
